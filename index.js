0e168c858ddce1aeab0bfe4e199ce26a4b01f162
khangdzvcl224-creator <khangdzvcl224@gmail.com>
2026-03-07 16:56:17 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log("Bot online");
+});
+
+client.on("messageCreate", message => {
+  if (message.content === ".ping") {
+    message.reply("Pong 🏓");
+  }
+});
+
+client.login(process.env.TOKEN);
